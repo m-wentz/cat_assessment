a8027d870a34ce64073b540a0dc6471c059f854b
Michael Wentz <mwentz@protonmail.com>
2024-01-17 12:48:40 -0700

    modified:   cat_assessment.py

--- cat_assessment.py
+++ cat_assessment.py
@@ -1,8 +1,13 @@
 import re
 
 # Applicant: Michael Wentz
-# Script to extract port numbers from Windows and Firewall logs
+# Script to extract port numbers from Password, Windows and Firewall logs
 # NOTE: Assumes consistent log pattern for each index.
+
+# Failed password log sample
+pass_log = """
+[redacted]
+"""
 
 # Windows log sample
 win_log = """
@@ -15,10 +20,14 @@ firewall_log = """
 """
 
 # Regex patterns for extracting ports
+pass_log_pattern = r'port (\d+)'
 win_port_pattern = r'Client Port: (\d+)'
-firewall_port_pattern = r's_port:"(\d+)"'
+firewall_port_pattern_1 = r's_port:"(\d+)"'
+firewall_port_pattern_2 = r'service:"(\d+)"'
 
 # Extracting ports from logs and display
-all_ports = re.findall(firewall_port_pattern, firewall_log) + \
-    re.findall(win_port_pattern, win_log)
+all_ports = re.findall(pass_log_pattern, pass_log) + \
+    re.findall(win_port_pattern, win_log) + \
+    re.findall(firewall_port_pattern_1, firewall_log) + \
+    re.findall(firewall_port_pattern_2, firewall_log)
 print("Ports Found: ", all_ports)
